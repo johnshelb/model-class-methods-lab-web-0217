ab2d423b2d79464be99ef4501c0de674850baa89
johnshelb <jsnd11@gmail.com>
2017-02-21 15:28:06 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,35 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where ("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)  #Boat.order("name desc").limit(3) also works as does order("name desc").limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+   def self.sailboats
+    joins(:classifications).where("classifications.name = ?",'Sailboat')
+   end
+   #joins(:classifications).where(classifications: {name: "Sailboat"})
+   #includes(:classifications).where(classifications: {name: "Sailboat"})  using includes instead of joins gets rid of n+1 problem
+
+   def self.with_three_classifications
+     joins(:classifications).group("boats.name").having("count(*) = ?",3)
+     #joins(:boat_classifications).group(:name).having("count(*) = ?",3) also works
+   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,22 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.catamaran_operators
+      joins(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
+  end
+
+  def self.sailors
+      joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def  self.talented_seamen
+    mb=joins(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq.pluck(:id)
+    s=joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq.pluck(:id)
+    where(id: (mb&s))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
